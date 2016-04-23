b182ff15b24ed17a59ba39202187854ff661cf4a
Gil Domingues <gmdz95@gmail.com>
2016-04-24 00:56:21 +0100

    configuration activity

---
+++ app/src/main/java/com/medctrl/mdteam/medctrl/activities/ConfigurationActivity.java
@@ -0,0 +1,24 @@
+package com.medctrl.mdteam.medctrl.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.medctrl.mdteam.medctrl.R;
+
+public class ConfigurationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+    }
+
+}
