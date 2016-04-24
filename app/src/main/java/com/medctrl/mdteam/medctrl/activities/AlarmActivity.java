a687229304b145093b149ca452a51f8d85ebe78a
Gil Domingues <gmdz95@gmail.com>
2016-04-24 02:15:39 +0100

    configuration activity done

--- app/src/main/java/com/medctrl/mdteam/medctrl/activities/AlarmActivity.java
+++ app/src/main/java/com/medctrl/mdteam/medctrl/activities/AlarmActivity.java
@@ -5,24 +5,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
-import android.widget.EditText;
 
 import com.medctrl.mdteam.medctrl.R;
 
-public class ConfigurationActivity extends AppCompatActivity {
+public class AlarmActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_configuration);
+        setContentView(R.layout.activity_alarm);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-
     }
 
 }
